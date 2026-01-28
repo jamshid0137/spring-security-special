7fe280f58ac3f1898a1b0e4de3f25cc3b2145479
MadaminovJamshidbek <jmadaminov.unlimited@gmail.com>
2026-01-28 14:08:02 +0500

    1.2 version

--- src/main/java/dasturlash/z/spring_security_multi_user/jamshid/Solution.java
+++ src/main/java/dasturlash/z/spring_security_multi_user/jamshid/Solution.java
@@ -1,4 +1,7 @@
 package dasturlash.z.spring_security_multi_user.jamshid;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println("jamshid o'zgartirdi !");
+    }
 }
